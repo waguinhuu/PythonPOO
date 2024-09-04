7f2fe54220f1c356d50fa30d172e8f441dfec817
Waguinho(ThisIsDoka) <wagnersillva909@gmail.com>
2024-09-04 20:56:20 -0300

    exercicio2

--- Heranca/exercicio2/exercicio2.py
+++ Heranca/exercicio2/exercicio2.py
@@ -1,6 +1,4 @@
 from abc import ABC,abstractmethod
-from cgitb import reset
-
 
 class Endereco:
     def __init__(self, logradouro: str, numero: str,
@@ -16,7 +14,7 @@ class Endereco:
         return (
             f"\nLogradouro: {self.logradouro}"
             f"\nNumero: {self.numero}"
-            f"\ncomplemento {self.complemento}"
+            f"\nComplemento {self.complemento}"
             f"\nCep: {self.cep}"
             f"\nCidade: {self.cidade}"
         )
@@ -41,15 +39,15 @@ class Funcionario(ABC):
                 f"\nTelefone: {self.telefone}"
                 f"\nEmail: {self.email}"
                 f"\nSalario: {self.salario}"
+                f"\nSalario final: {self.salario_final()}"
                 f"\nEndereço: {self.endereco}"
-                f"\nSalario: {self.salario_final()}"
                 )
 
 class Engenheiro(Funcionario):
     BONIFICACAO = 1.1
 
     def __init__(self, nome: str, telefone: str, email: str,salario: float, endereco: Endereco, crea: str) -> None:
-        super.__init__(nome, telefone, email,salario, endereco)
+        super().__init__(nome, telefone, email,salario, endereco)
         self.crea = crea
 
     def salario_final(self) -> float:
@@ -59,11 +57,35 @@ class Engenheiro(Funcionario):
     def __str__(self) -> str:
         return (
             f"{super().__str__()}"
-            f"\nCrea: {self.crea}"
+            f"\nCrea: {self.crea}\n"
         )
 
 
 
 class Medico(Funcionario):
+    BONIFICACAO = 1.3
+
     def __init__(self,nome: str, telefone: str, email: str,salario: float, endereco: Endereco, crm: str) -> None:
+        super().__init__(nome,telefone,email,salario,endereco)
         self.crm = crm
+
+    def salario_final(self) -> float:
+        resultado = self.salario * self.BONIFICACAO
+        return resultado
+
+    def __str__(self) -> str:
+        return (
+                f"{super().__str__()}"
+                f"\nCRM: {self.crm}"
+        )
+
+
+
+engenheiro = Engenheiro("Wagner","7983445733","wg@gmail.com",1000,
+                        Endereco("Rua A","88","sla","43900-000","SFC"),"2321")
+
+medico = Medico("Paulo","719383392","pl@gmail.com",1000,
+                Endereco("Rua B","00","nnsei","28900-000","STA"),"92981")
+
+print(engenheiro)
+print(medico)
